d8c1acbb438e0d24033129762dbd0222a1171446
Rajasekar <rajasekar241996@gmail.com>
2021-06-19 22:19:41 +0530

    Updated code

--- Smart-Charge.py
+++ Smart-Charge.py
@@ -2,7 +2,10 @@ import psutil
 import tinytuya
 import csv
 import datetime
+from win10toast import ToastNotifier
+import emoji
 
+toaster = ToastNotifier()
 debugMode = True
 action = "No Action"
 statusCode = "No Request Made"
@@ -12,7 +15,15 @@ def plugRequest(switchState):
     d = tinytuya.OutletDevice('52585621c4dd571b8db0', '192.168.29.98', '32ba71b35d1df9a4')
     d.set_version(3.3)
     d.status()
+    data = d.status()
+    # if data['Error'] & data['Error'] == 'Network Error: Device Unreachable':
+    #     toaster.show_toast("Smart-Charge", "Pls turn on Smart-plug")
+    # else:
     d.set_status(switchState)
+    if switchState:
+        d.set_timer(5400)  # 1hour 30 mins is 5400secs
+    txt = "charging :battery::electric_plug:" if switchState else "Charged :battery:"
+    toaster.show_toast("Smart-Charge", emoji.emojize(txt))
 
 
 battery = psutil.sensors_battery()
@@ -24,13 +35,15 @@ if not pluggedIn:
 else:
     chargeState = "Plugged In"
 print(f"{str(currentCharge)}% | {chargeState}")
+if debugMode:
+    toaster.show_toast("Smart-Charge", f"{str(currentCharge)}% | {chargeState}")
 
 if pluggedIn and currentCharge >= 80:
     # unplug laptop
     print("Turning off plug")
     action = "Turn Off"
     plugRequest(False)
-elif not pluggedIn and currentCharge < 35:
+elif not pluggedIn and currentCharge < 30:
     # plug laptop in
     print("Turning on plug")
     action = "Turn On"
